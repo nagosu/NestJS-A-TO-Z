de6421181d832303ea162058728ac0492619c534
nagosu <nozick1021@gmail.com>
2024-11-01 22:28:12 +0900

    2024.11.01 Interceptors & AOP 패턴까지 학습

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,7 +1,57 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  HttpException,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Put,
+  UseFilters,
+  UseInterceptors,
+} from '@nestjs/common';
 import { CatsService } from './cats.service';
+import { HttpExceptionFilter } from 'src/common/exceptions/http-exception.filter';
+import { PositiveIntPipe } from 'src/common/pipes/positiveInt.pipe';
+import { SuccessInterceptor } from 'src/common/interceptors/success.interceptor';
 
 @Controller('cats')
+@UseInterceptors(SuccessInterceptor)
+@UseFilters(HttpExceptionFilter)
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
+
+  @Get()
+  getAllCat() {
+    console.log('hello controller');
+    return {
+      cats: 'get all cats api',
+    };
+  }
+
+  @Get(':id')
+  getOneCat(@Param('id', ParseIntPipe, PositiveIntPipe) param: number) {
+    return '';
+  }
+
+  @Post()
+  createCat() {
+    return '';
+  }
+
+  @Put(':id')
+  updateCat() {
+    return '';
+  }
+
+  @Patch(':id')
+  updatePartialCat() {
+    return '';
+  }
+
+  @Delete(':id')
+  deleteCat() {
+    return '';
+  }
 }
---
+++ src/common/pipes/positiveInt.pipe.ts
@@ -0,0 +1,16 @@
+import {
+  ArgumentMetadata,
+  HttpException,
+  Injectable,
+  PipeTransform,
+} from '@nestjs/common';
+
+@Injectable()
+export class PositiveIntPipe implements PipeTransform {
+  transform(value: number) {
+    if (value < 0) {
+      throw new HttpException('value < 0', 400);
+    }
+    return value;
+  }
+}
