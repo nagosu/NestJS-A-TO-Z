b12bd749f56d815364a083932eb817fe16d3ff71
nagosu <nozick1021@gmail.com>
2024-11-03 16:50:35 +0900

    comments module, controller, service 생성

--- src/comments/services/comments.service.spec.ts
+++ src/comments/services/comments.service.spec.ts
@@ -1,15 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { CatsService } from './cats.service';
+import { CommentsService } from './comments.service';
 
-describe('CatsService', () => {
-  let service: CatsService;
+describe('CommentsService', () => {
+  let service: CommentsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CatsService],
+      providers: [CommentsService],
     }).compile();
 
-    service = module.get<CatsService>(CatsService);
+    service = module.get<CommentsService>(CommentsService);
   });
 
   it('should be defined', () => {
